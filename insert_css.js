7a7898aeade47de4769742bf8bb4b6a0d0d6018f
dsts </dev/null>
2016-04-03 10:54:14 +0100

    Does exactly what I programmed it to do

---
+++ insert_css.js
@@ -0,0 +1,8 @@
+var stylesheet = "https://s.4cdn.org/css/md2016.651.css";
+
+var link = document.createElement('link');
+link.setAttribute("rel", "stylesheet");
+link.setAttribute("type", "text/css");
+link.setAttribute("href", stylesheet);
+
+document.head.appendChild(link);
